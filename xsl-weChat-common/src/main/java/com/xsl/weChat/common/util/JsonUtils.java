d23012fb54c569a38511465f65fe270ee38861e2
LJW <1037893644@qq.com>
2019-04-16 22:49:13 +0800

    初始化配置悬赏令微信后台系统

---
+++ xsl-weChat-common/src/main/java/com/xsl/weChat/common/util/JsonUtils.java
@@ -0,0 +1,78 @@
+package com.xsl.weChat.common.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+
+/**
+ * @ClassName:JsonUtils
+ * @descrription:悬赏令json工具
+ * @version V1.0
+ * @author:何林鸿
+ * @date: 2018年7月29日
+ */
+public class JsonUtils {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    /**
+     * 将对象转换成json字符串。
+     * <p>Title: pojoToJson</p>
+     * <p>descrription: </p>
+     * @param data
+     * @return
+     */
+    public static String objectToJson(Object data) {
+    	try {
+    		//将对象转换为json串
+			String string = MAPPER.writeValueAsString(data);
+			return string;
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+    	return null;
+    }
+    
+    /**
+     * 将json结果集转化为对象
+     * 
+     * @param jsonData json数据
+     * @param clazz 对象中的object类型
+     * @return
+     */
+    public static <T> T jsonToPojo(String jsonData, Class<T> beanType) {
+        try {
+        	//将json转换为bean
+            T t = MAPPER.readValue(jsonData, beanType);
+            return t;
+        } catch (Exception e) {
+        	e.printStackTrace();
+        }
+        return null;
+    }
+    
+    /**
+     * 将json数据转换成pojo对象list
+     * <p>Title: jsonToList</p>
+     * <p>descrription: </p>
+     * @param jsonData
+     * @param beanType
+     * @return
+     */
+    public static <T>List<T> jsonToList(String jsonData, Class<T> beanType) {
+    	//构造list的java类型
+    	JavaType javaType = MAPPER.getTypeFactory().constructParametricType(List.class, beanType);
+    	try {
+    		//转换为pojo对象list
+    		List<T> list = MAPPER.readValue(jsonData, javaType);
+    		return list;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	
+    	return null;
+    }
+    
+}
