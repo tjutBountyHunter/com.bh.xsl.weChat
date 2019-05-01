bf54026a9a8ea93814827e6ed1c2e66c857effd3
LJW <1037893644@qq.com>
2019-05-01 19:35:57 +0800

    测试Jenkins

--- xsl-weChat-service/src/main/java/com/xsl/weChat/service/impl/XslGetWeChatServiceImpl.java
+++ xsl-weChat-service/src/main/java/com/xsl/weChat/service/impl/XslGetWeChatServiceImpl.java
@@ -10,6 +10,7 @@ import com.xsl.wechat.mapper.XslMasterMapper;
 import com.xsl.wechat.mapper.XslUserMapper;
 import com.xsl.wechat.pojo.*;
 import com.xsl.wechat.vo.WeChatAccountConfig;
+import net.sf.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,7 +51,7 @@ public class XslGetWeChatServiceImpl implements XslGetWeChatService {
             String WX_URL = "https://api.weixin.qq.com/sns/jscode2session?" +
                     "appid=wxb7715d658f00b0e1&secret=d95be64495bf3a94114bae2841a54961&js_code=" + code +"&"+
                     "grant_type=authorization_code";
-            net.sf.json.JSONObject jsonObject = CommonUtil.httpsRequest(WX_URL, "GET", null);
+            JSONObject jsonObject = CommonUtil.httpsRequest(WX_URL, "GET", null);
             if(jsonObject!=null){
                 String userId = (String) jsonObject.get("openid");
                 WeChatAccountConfig weChatAccountConfig = new WeChatAccountConfig();
